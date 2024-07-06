940e6ce925fbd248f08f43fd6295157bee8f38ef
Akulaarunkumar <arunkumarakula58@gmail.com>
2024-07-06 10:31:31 +0530

    specialvar

--- specialvar.sh
+++ specialvar.sh
@@ -4,3 +4,11 @@ VAR1=$1
 VAR2=$2
 
 echo "variable1: $VAR1"
+
+echo "scrpt name: $0" #it will print the script name
+
+echo "varibale1: $VAR1"
+
+echo "all : $@" #it will print all the variables
+
+echo "numbers of vars: $#" #it shows no.of variables
