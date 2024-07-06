9168726831a411e667a814bd22a4e9a4c64aadcd
Akulaarunkumar <arunkumarakula58@gmail.com>
2024-07-06 10:20:09 +0530

    function

---
+++ function.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0]
+    then
+    echo "Installation is...Failed"
+    exit 1
+    elseecho "Installation...success"
+    fi
+}
+
+if [ $USERID -ne 0]
+then
+echo "Error:: please run this script with root access"
+exit 1
+fi
+
+yum install postfix -y
+
+VALIDATE $?
+
+yum install maven -y
+
+VALIDATE $?
