1be8916fa4a8f3297c14f755273066c09c8496ef
Akulaarunkumar <arunkumarakula58@gmail.com>
2024-07-06 09:06:09 +0530

    cond2

--- conditional2.sh
+++ conditional2.sh
@@ -5,8 +5,23 @@ USERID=$(id -u)
 if [$USERID -ne 0]
 then
 
-echo "Error:please run the script with the root access"
+echo "Error::please run the script with the root access"
 
 fi
 
-yum install mysql -y
+yum install postfix -y
+if [ $? -ne 0]
+then
+ 
+ echo "Installation of postfix is error"
+ else
+ echo "Installation of postfix is sucess"
+
+ fiyum install maven -y
+ if [ $? -ne 0]
+ then
+  echo "Installation of maven is error"
+  else
+  echo installation of maven is sucess"
+
+  fi
