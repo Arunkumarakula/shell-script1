402e899ec628e6cc3d2ea57586c4520adf8253a3
Akulaarunkumar <arunkumarakula58@gmail.com>
2024-07-06 09:38:55 +0530

    cond2

--- conditional2.sh
+++ conditional2.sh
@@ -9,24 +9,22 @@ echo "Error:: please run the script with the root access"
 
 fi
 
-yum remove postfix -y
+yum install postfix -y
 if [ $? -ne 0 ]
 then
  
- echo "Installation of postfix is error(not removed)"
+ echo "Installation of postfix is error"
  exit 1
  else
  echo "Installation of postfix is sucess"
-
  fi
 
- yum remove maven -y
+ yum install maven -y
  if [ $? -ne 0 ]
  then
 
-  echo "Installation of maven is error(not removed)"
+  echo "Installation of maven is error"
   exit 1
   else
   echo installation of maven is sucess"
-
   fi
